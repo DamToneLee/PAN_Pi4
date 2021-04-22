db0311e1b49526d1b674312e6dfc0d7b74ce553f
DamToneLee <73567631+DamToneLee@users.noreply.github.com>
2021-04-22 14:49:50 +0800

    4/22 Verison 2.1

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -3,7 +3,8 @@
 echo "preinstall packages download"
 apt-get update
 apt-get upgrade
-apt-get install vim hostapd openvswitch-switch
+apt-get -y install vim hostapd openvswitch-switch
+apt-get -y install bridge-utils bluez python-dbus python-gobject
 #git clone https://github.com/DamToneLee/PAN_Pi4.git
 echo "preinstall packages has been downloaded."
 
@@ -14,7 +15,7 @@ cp ./blueagent5.py ./bt-pan.py /usr/local/bin/
 chmod 755 /usr/local/bin/*.py
 ln -s /usr/local/bin/blueagent5.py /usr/local/bin/blueagent5
 ln -s /usr/local/bin/bt-pan.py /usr/local/bin/bt-pan
-vim /etc/network/interfaces
+cp ./interfaces /etc/network/interfaces
 cp blueagent5.service /etc/systemd/system/blueagent5.service
 systemctl enable blueagent5
 cp bt-pan.service /etc/systemd/system/bt-pan.service
@@ -25,12 +26,12 @@ cp pan-network.sh /usr/local/bin/pan-network.sh
 chmod 755 /usr/local/bin/pan-network.sh
 cp pan-network.service /etc/systemd/system/pan-network.service
 systemctl enable pan-network
-vim /etc/sysctl.conf
+cp ./sysctl.conf /etc/sysctl.conf
 service bluetooth restart
 echo "bluetooth PAN setting done."
 
 echo "WiFi AP setting"
-cp hostapd.conf ~/hostapd.conf
+cp ./hostapd.conf ~
 echo "WiFi AP setting done."
 echo "all packages installed."
 exit 0
